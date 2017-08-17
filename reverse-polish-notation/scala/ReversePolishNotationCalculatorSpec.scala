2304670570e8a1e22ddbd57db306cd4a605546cd
mrodrigues <gmrodrigues@gmail.com>
2017-08-17 21:53:50 +0100

    Fixing the description of the tests

--- reverse-polish-notation/scala/ReversePolishNotationCalculatorSpec.scala
+++ reverse-polish-notation/scala/ReversePolishNotationCalculatorSpec.scala
@@ -3,7 +3,7 @@ import org.scalatest._
 
 class ReversePolishNotationCalculatorSpec extends FlatSpec with Matchers {
 
-    "An NPN calculator" should "return 10 for 5 5 +" in {
+    "A reverse polish notation calculator" should "calculate a valid value in a simple scenario" in {
         val calculator = new ReversePolishNotationCalculator()
 
         val result = calculator.evaluate("5 5 +")
@@ -12,7 +12,7 @@ class ReversePolishNotationCalculatorSpec extends FlatSpec with Matchers {
 
     }
 
-    "An NPN calculator" should "return 15 for 5 5 5 + +" in {
+    "A Reverse polish notation calculator" should "calculate a valid value for a larger expression" in {
         val calculator = new ReversePolishNotationCalculator()
 
         val result = calculator.evaluate("5 5 5 + +")
@@ -21,7 +21,7 @@ class ReversePolishNotationCalculatorSpec extends FlatSpec with Matchers {
 
     }
 
-    "An NPN calculator" should "return 5 for complex notation 15 7 1 1 + - ÷ 3 × 2 1 1 + + -" in {
+    "A Reverse polish notation calculator" should "return 5 for complex notation 15 7 1 1 + - ÷ 3 × 2 1 1 + + -" in {
         val calculator = new ReversePolishNotationCalculator()
 
         val result = calculator.evaluate("15 7 1 1 + - ÷ 3 × 2 1 1 + + -")
