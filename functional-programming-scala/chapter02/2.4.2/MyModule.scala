4d70b47c480d03be48a7f90cedb1940d1e05dee7
mrodrigues <gmrodrigues@gmail.com>
2017-08-30 20:46:08 +0100

    adding formatResult function that is an higher order function

--- functional-programming-scala/chapter02/2.4.2/MyModule.scala
+++ functional-programming-scala/chapter02/2.4.2/MyModule.scala
@@ -18,7 +18,7 @@ object MyModule {
     }
 
     private def formatAbs(x: Int) = {
-        val msg = "The absolute number of %d is %d."
+        val msg = "The absolute value of %d is %d."
         msg.format(x, abs(x))
     }
 
@@ -27,9 +27,20 @@ object MyModule {
         msg.format(n, factorial(n))
     }
 
+    private def formatResult(name: String, n: Int, f: Int => Int) = {
+        val msg = "The %s of %d is %d."
+        msg.format(name, n, f(n))
+    }
+
     def main(args: Array[String]): Unit = {
         println(formatAbs(-42))
         println(formatFactorial(7))
+
+        println("Printing the same using formatResult as an Higher Order Function")
+
+        println(formatResult("absolute value", -42, abs))
+        println(formatResult("factorial", 7, factorial))
+
     }
 
 }
